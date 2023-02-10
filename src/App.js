09d52d8297cdf2dccb48e759c5f6d4597a0421eb
Faris101 <doricfaris50@gmail.com>
2023-02-10 19:57:13 +0100

    search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import SearchIcon from './search.svg'
 import MovieCard from './MovieCard';
@@ -20,11 +20,13 @@ const movie1={
 
 
 const App= ()=> {
+  const [movies, setMovies]=useState([]);
+  const [searchTerm, setSearchTerm]=useState('');
   const searchMovies=async(title)=>{
     const response=await axios.get(`${API_URL}&s=${title}`);
     const data=response.data;
     
-    console.log(data);
+    setMovies(data.Search);
     
   }
   useEffect(()=>{
@@ -36,14 +38,25 @@ const App= ()=> {
 
     <div className="search">
       <input placeholder='Search for movies'
-      value="Superman"
-      onChange={()=>{}}
+      value={searchTerm}
+      onChange={(e)=>setSearchTerm(e.target.value)}
       />  
-      <img src={SearchIcon} alt="search" onClick={()=>{}}/>
+      <img src={SearchIcon} alt="search" onClick={()=>searchMovies(searchTerm)}/>
     </div>  
-     <div className='container'>
-        <MovieCard movie1={movie1}/>
-      </div>
+    {/* {
+      movies?.lenght>0 
+      ?(
+      ):(
+        <div className='empty'> 
+            <h2>No movies found</h2>
+        </div>
+      )
+    } */}
+      <div className='container'>
+          {movies?.map((movie)=>(
+             <MovieCard movie={movie}/>
+            ))}
+        </div>
     </div>
   );
 }
